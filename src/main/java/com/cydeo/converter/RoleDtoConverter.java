8398139819ba6a235e7c320dbf9397bee5b76371
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-26 15:49:00 +0300

    ticketing-project-data-1 update 8

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -3,6 +3,7 @@ package com.cydeo.converter;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,7 @@ public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     RoleService roleService;
 
-    public RoleDtoConverter(RoleService roleService) {
+    public RoleDtoConverter(@Lazy RoleService roleService) {
         this.roleService = roleService;
     }
 
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,18 +2,22 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
 
@@ -25,7 +29,14 @@ public class RoleServiceImpl implements RoleService {
         // Go to repository and find a service(method) which gives me the roles from db
         // How I will call any service here?
 
-        return null;
+        List<Role> roleList = roleRepository.findAll();
+
+        // I have Role entities from DB
+        // I need to convert those Role entities to DTOs
+        // I need to use ModelMapper
+        // I already created a class called RoleMapper and there are methods for me that will make this conversion
+
+        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
