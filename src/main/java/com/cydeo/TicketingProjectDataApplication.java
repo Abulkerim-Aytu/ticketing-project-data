abce92847d2be28119dbc29d806a3dc41d2911df
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-26 14:42:36 +0300

    ticketing-project-data-1 update 6

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -2,12 +2,24 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
 
-@SpringBootApplication
+@SpringBootApplication // this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
+
+    // WHEN WE USE THIRD PARTY LIBRARY YOU NEED TO CREATE THE @BEAN
+    // I am trying to add bean in the container throw @Bean annotation
+    // Create a class annotated with @Configuration
+    // Write a method which return the object that you trying to add in the container
+    // Annotate this method with @Bean
+
+
+
+
+
 
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,21 @@ import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
 
     @Override
     public List<RoleDTO> listAllRoles() {
+
+        // Controller called me and requestiond all RoleDTOs so it can show in the drop-down in the UI
+        // I need to make a call to db and get all the roles from table
+        // Go to repository and find a service(method) which gives me the roles from db
+        // How I will call any service here?
+
         return null;
     }
 
