5ba63cb4c8eaaf131114d7d2d1f7546003bc4b44
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-26 13:23:09 +0300

    ticketing-project-data-1 update 5

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Override
+    public List<RoleDTO> listAllRoles() {
+        return null;
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<UserDTO> listAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDTO findByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public void save(UserDTO user) {
+
+    }
+
+    @Override
+    public void deleteByUserName(String username) {
+
+    }
+}
