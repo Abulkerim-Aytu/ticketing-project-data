56d7a4dee7b8fbb2c561699c98dbf18daa4b13fe
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-28 10:48:11 +0300

    ticketing-project-data-3 update 6

--- src/main/java/com/cydeo/mapper/GenericMapper.java
+++ src/main/java/com/cydeo/mapper/GenericMapper.java
@@ -4,19 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GenericMapper<D, E> {
+public interface GenericMapper<D, E> {
 
-    private final ModelMapper modelMapper;
+    public E convertToEntity(D dto, Class<E> entityClass);
 
-    public GenericMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
+    public D convertToDto(E entity, Class<D> dtoClass);
 
-    public E convertToEntity(D dto, Class<E> entityClass) {
-        return modelMapper.map(dto, entityClass);
-    }
-
-    public D convertToDto(E entity, Class<D> dtoClass) {
-        return modelMapper.map(entity, dtoClass);
-    }
 }
