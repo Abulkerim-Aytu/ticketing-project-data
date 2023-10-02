33f33df7ce3877592362b4e0c016564efcf32770
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-10-02 13:44:53 +0300

    ticketing-project-data-4 update 1

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,40 @@
+package com.cydeo.entity;
+
+
+import com.cydeo.enums.Status;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "tasks")
+@Where(clause = "is_deleted=false")
+public class Task extends BaseEntity{
+
+        private String taskSubject;
+
+        private String taskDetail;
+
+        @Enumerated(EnumType.STRING)
+        private Status taskStatus;
+
+        @Column(columnDefinition = "DATE")
+        private LocalDate assignedDate;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id")
+        private User assignedEmployee;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "project_id")
+        private Project project;
+    }
+
+
